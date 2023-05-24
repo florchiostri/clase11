5aae2d91e8a3943ec5c28b485398b4c67b57d45a
Flor Chiostri <130665833+florchiostri@users.noreply.github.com>
2023-05-23 23:34:39 -0300

    heladeria.js
    ejercitacion 11

---
+++ heladeria.js
@@ -0,0 +1,30 @@
+const helado = 230; //precio helado
+
+let topping = "Oreo";
+let precio;
+let precioFinal;
+
+if(topping == "Oreo"){
+    precio = 10;
+} else if(topping == "KitKat") {
+    precio = 15;
+} else if(topping == "Kinder") {
+    precio = 25;
+}
+
+
+precioFinal = helado + precio;
+
+switch(pedido-cliente) {
+    case x: "carne"
+      console.log("vino tinto");
+      break;
+    case y: "verdura"
+        console.log("agua");
+      break;
+    case s: "pescado"
+       console.log("vino blanco");
+       break;
+    default:
+        console.log("elija carne, pescado o verdura")
+  }
